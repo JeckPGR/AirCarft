be466e391f7cf143d52150c9201124ab1b4051ae
Ahmad Dzaky Ar Razi <106154895+JeckPGR@users.noreply.github.com>
2024-03-19 01:03:44 +0700

    Update servicespage.jsx
    first update servicespage

--- src/pages/servicespage.jsx
+++ src/pages/servicespage.jsx
@@ -2,8 +2,12 @@ import underconstruct from "/images/underconstruct.jpg";
 const services = () => {
   return (
     <div className="min-h-screen flex flex-col gap-y-4 justify-center items-center bg-yellow-200/20">
-      <img src={underconstruct} alt="wait" className="w-[500px]" />
-      <h1 className=" text-5xl font-semibold ">
+      <img
+        src={underconstruct}
+        alt="wait"
+        className=" w-72 md:w-80 lg:w-[500px]"
+      />
+      <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-semibold ">
         Sorry this page Under construction
       </h1>
     </div>
